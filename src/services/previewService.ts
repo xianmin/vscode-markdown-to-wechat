4d79b3bc2ab7fd6076151f2649a41f468fa1c752
Chen Xianmin <xianmin12@gmail.com>
2025-04-18 22:11:24 +0800

    feat: 为微信公众号预览功能添加图标，优化WebView上下文管理

--- src/services/previewService.ts
+++ src/services/previewService.ts
@@ -76,8 +76,12 @@ export class PreviewService implements IPreviewService {
         enableScripts: true,
         // 限制WebView可以加载的资源范围
         localResourceRoots: [vscode.Uri.joinPath(this.extensionUri, 'media')],
+        retainContextWhenHidden: true, // 隐藏时保留上下文，减少重新加载问题
       }
     )
+
+    // 存储当前内容以便在 WebView 准备好时使用
+    const initialContent = markdownContent
 
     // 设置WebView的HTML内容
     this.previewPanel.webview.html = WebviewContentProvider.getWebviewContent(
@@ -86,7 +90,7 @@ export class PreviewService implements IPreviewService {
     )
 
     // 注册消息处理
-    this.registerMessageHandlers()
+    this.registerMessageHandlers(initialContent)
 
     // 监听面板关闭事件
     this.previewPanel.onDidDispose(() => {
@@ -140,8 +144,9 @@ export class PreviewService implements IPreviewService {
 
   /**
    * 注册消息处理
+   * @param initialContent 初始的 Markdown 内容，用于 WebView 准备好时发送
    */
-  public registerMessageHandlers(): void {
+  public registerMessageHandlers(initialContent?: string): void {
     if (!this.previewPanel) {
       return
     }
@@ -151,7 +156,11 @@ export class PreviewService implements IPreviewService {
       switch (message.type) {
         case 'webviewReady':
           // WebView已准备好，发送Markdown内容
-          if (vscode.window.activeTextEditor && this.previewPanel) {
+          if (initialContent) {
+            // 使用传入的初始内容
+            this.updatePreview(initialContent)
+          } else if (vscode.window.activeTextEditor && this.previewPanel) {
+            // 否则尝试使用当前编辑器内容
             this.updatePreview(vscode.window.activeTextEditor.document.getText())
           }
           break
